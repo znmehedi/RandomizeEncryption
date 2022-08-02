e119013f5fd646e06030218c41f1b0a1b9f2b704
znmehedi <zn.mehedi@gmail.com>
2022-08-02 22:01:54 +0600

    Random and Encryption-Decryption added

---
+++ RandomizeEncryption/RandomizeEncryption/Cryptos/ICrypto.cs
@@ -0,0 +1,8 @@
+﻿namespace RandomizeEncryption.Cryptos
+{
+    public interface ICrypto
+    {
+        string? Encrypt(string plainText, string privateKey);
+        string? Decrypt(string cipherText, string privateKey);
+    }
+}
---
+++ RandomizeEncryption/RandomizeEncryption/Randoms/Randomer.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+
+namespace RandomizeEncryption.Randoms
+{
+    public class Randomer : IRandomer
+    {
+        public string? GetUniqueKey(string pattern, int size)
+        {
+            if (pattern == null || pattern.Length <= 0 || size <= 0)
+                return null;
+            char[] chars = pattern.ToCharArray();
+
+            byte[] data = new byte[4 * size];
+            using (var crypto = RandomNumberGenerator.Create())
+            {
+                crypto.GetBytes(data);
+            }
+            StringBuilder result = new StringBuilder(size);
+            for (int i = 0; i < size; i++)
+            {
+                var rnd = BitConverter.ToUInt32(data, i * 4);
+                var idx = rnd % chars.Length;
+
+                result.Append(chars[idx]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
